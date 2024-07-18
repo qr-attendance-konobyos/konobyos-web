f943bbab59c29f62a266a5dbc6adf82b0de25beb
Zemichael Tesfaye <zzimick@gmail.com>
2024-07-19 00:40:30 +0300

    pwa enabled

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    // VitePWA({
-    //   registerType: "autoUpdate",
-    //   // devOptions: {
-    //   //   enabled: true,
-    //   // },
-    // }),
+    VitePWA({
+      registerType: "autoUpdate",
+    }),
   ],
 });

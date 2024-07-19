63732e4e150e22ec7de06c191c3ecb8d41f5e59b
Zemichael Tesfaye <zzimick@gmail.com>
2024-07-19 13:04:49 +0300

    home page themed

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,14 +1,17 @@
 import { useUser } from "../api/user";
 import { useAuth } from "../context";
+import "./home.scss";
 
 export function Home() {
   const { logout } = useAuth();
   const { data: user } = useUser();
   return (
-    <>
+    <div className="home">
       <h1> Home Page</h1>
       <h2>Hello {user?.name}</h2>
-      <button onClick={() => logout()}>logout</button>
-    </>
+      <button className="button primary" onClick={() => logout()}>
+        Logout
+      </button>
+    </div>
   );
 }
